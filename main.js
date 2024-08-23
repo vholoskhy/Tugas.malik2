a6c39f23d2770dbf5eea690b29410d7a7c420e91
koliscom <vholoskhy@gmail.com>
2024-08-23 13:50:06 +0700

    bikinTugas

---
+++ main.js
@@ -0,0 +1,64 @@
+// const = (1,2,3,4,5)
+// for  (let i =0;1 <Array.length;i++){
+//     console.log(
+//         Array[i+1]
+//     );
+// }
+
+// const array1 = [1,2,3,4,5,6]
+// const arrayyangDikali2 = []
+// for (let i =0;1<array1.length;1++){
+//     arrayyangDikali2[i]=1 
+// }
+
+// const array1 = [1,2,3,4,5,6]
+// const hasilnya = array1.map(function(item)) {
+//     console.log(item);
+
+//     const jadilist = array1.map(function(){
+//         console.log();
+//     })
+// }
+
+
+// const array1 =[1,2,3,4,5,6]
+// array1.filter = array.filter (function(item){
+//     return item > 3
+// })
+
+// // -----
+
+// const product = [
+//     {
+//        name : 'baju hitam'
+//        harga:3500
+//        diskon:3000
+//     }
+//     {
+//        name :'kaos putih'
+//        harga :5300
+//        diskon :5000
+
+//     }
+//     {
+//         name :'celana pendek'
+//         harga : 7700
+//         diskon:7000
+//     }
+// ]
+
+    
+// //     const munculDiskon = product.map(function(para){
+// //         return.para.harga - para.diskon
+// //     }
+// //     console.log(munculDiskon);
+
+
+// // )
+
+// const semuaProduct = product.map(function(item){
+//     return "<h1>"+ jadiKapital.name.toUpperCase()+ "Rp,"
+// })
+// const stringAja
+
+let     
